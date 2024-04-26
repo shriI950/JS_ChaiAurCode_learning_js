102a628bc4f831144648d83d97cb0c9c8d3f2291
Shrirang Joshi <51706802+shriI950@users.noreply.github.com>
2024-04-26 12:23:10 +0000

    DataTypes

---
+++ 101_Basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log(3 + 3)
+// code readability should be high. (Code should be easier to read.)
+
+let name = "Shrirang"
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to the power of 52.
+// bigint => for big numbers bigger than number range.
+// string => written in double quotes.
+// boolean => true/false value.
+// null => standalone value
+// undefined => value used for variables which are declared but not
+            //  defined.
+// symbol => Is a datatype used to uniquely identify different
+            //  components.
+// object => this is a datatype which is an object, i.e. instance of 
+            //  the variable
+
+console.log(typeof null);
+console.log(typeof true);
+console.log(typeof undefined);
